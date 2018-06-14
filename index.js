229aaa1b2e28a616635bb555b81ddc57459a680b
Mahto <sandiper30@gmail.com>
2018-06-14 17:51:31 +0530

    resa

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var lib = require('./lib');
+
+/*var obj = new lib.student(12,"Nike");
+
+obj.display();
+lib.global(); */
+
+var home= function(){
+console.log("This function runs in the home page...");
+}
+const app ="Global node package";
+
+module.exports={
+    home:home,
+    appname:app,
+	
+	lib:lib
+
+}
